59ff6591de3e2b723d710a9b0d38bf02583cdeab
mg <mgeng03@stevens.edu>
2020-09-27 20:13:59 -0400

    update

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -5,9 +5,12 @@ import numpy as np
 x = np.arange(0,2*np.pi,0.01)
 y_sin = np.sin(x)
 y_cos = np.cos(x)
-
+y_tan = np.tan(x)
 # plot the curves
 plt.plot(x,y_sin)
 plt.plot(x,y_cos)
+plt.plot(x,y_tan)
+
+plt.ylim(-1.1, 1.1)
 plt.show()
 
