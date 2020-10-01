c076dfb18586cb2787578602a0ff775d0f366587
ben062930 <70728503+ben062930@users.noreply.github.com>
2020-10-01 13:49:44 -0400

    Update HW1.py

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -14,3 +14,17 @@ plt.plot(x,y_tan)
 plt.ylim(-1.1, 1.1)
 plt.show()
 
+#The below is a revised version
+if __name__ == "__main__":
+    x = np.arange(0, 2 * np.pi, 0.001*np.pi)
+    #to make plot smoother
+    y_sin = np.sin(x)
+    y_cos = np.cos(x)
+    y_tan = np.tan(x)
+    # plot the curves
+    plt.plot(x, y_sin, color = "green", label = "sin")
+    plt.plot(x, y_cos, color = "blue", label = "cos")
+    plt.plot(x, y_tan, color = "red", label = "tan")
+
+    plt.ylim(-1.1, 1.1)
+    plt.show()  
