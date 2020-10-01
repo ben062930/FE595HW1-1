11baee3b925bde3c44942d9f3f167d268b6d75d7
ben062930 <70728503+ben062930@users.noreply.github.com>
2020-10-01 19:12:24 -0400

    Update HW1.py

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,21 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# set the period of 2*pi
-x = np.arange(0,2*np.pi,0.01)
-y_sin = np.sin(x)
-y_cos = np.cos(x)
-y_tan = np.tan(x)
-# plot the curves
-plt.plot(x,y_sin)
-plt.plot(x,y_cos)
-plt.plot(x,y_tan)
-
-plt.ylim(-1.1, 1.1)
-plt.show()
-
-#The below is a revised version
-if __name__ == "__main__":
+def hw1():
     x = np.arange(0, 2 * np.pi, 0.001*np.pi)
     #to make plot smoother
     y_sin = np.sin(x)
@@ -27,4 +13,8 @@ if __name__ == "__main__":
     plt.plot(x, y_tan, color = "red", label = "tan")
 
     plt.ylim(-1.1, 1.1)
-    plt.show()  
+    plt.show() 
+
+#The below is a revised version
+if __name__ == "__main__":
+ hw1()
